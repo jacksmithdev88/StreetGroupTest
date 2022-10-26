4de1b69e42effab892f8441a1d67c6083e448f73
Jack Smith <jacksmithdev88@gmail.com>
2022-10-26 22:59:01 +0100

    final version, addition of like button and different stylings

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,20 @@
 @section('content')
     <div class="flex flex-row flex-wrap">
         @foreach ($pictures as $picture)
-            <div class="rounded overflow-hidden bg-white border border-gray-200 mx-5 p-4">
+            <div class="rounded overflow-hidden bg-white border border-gray-200 mx-5 my-3 p-4">
                 <img width="400px" src="{{ asset('storage/' . $picture->file_path) }}" />
                 <p class="mt-2 text-gray-500">{{ $picture->name }}</p>
                 <p class="mt-2 text-gray-500">{{ $picture->votes }} votes</p>
                 <form action="{{ route('pictures.upvote', ['picture' => $picture->id]) }}" method="POST">
                     @csrf
-                    <button type="submit">Like</button>
+                    <button type="submit">
+                        <svg class="h-5 w-5 hover:text-blue-600" x-description="solid/thumb-up"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path
+                                d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z">
+                            </path>
+                        </svg>
+                    </button>
                 </form>
             </div>
         @endforeach
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,26 +7,35 @@
             @csrf
             <div class="p-4">
                 <label for="name">Name:</label>
-                <input type="text" name="name" />
+                <input type="text"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm mx-auto rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    name="name" />
             </div>
             <div class="p-4">
-                <label for="image">File:</label>
-                <input type="file" name="image" />
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="file_input">Upload
+                    file</label>
+                <input
+                    class="block w-1/2 px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none m-auto ease-in-out"
+                    id="image" name="image" type="file">
+                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF.</p>
             </div>
-            <div class="p-4">
+            <div class="errors">
+                @if ($errors->any())
+                    <div class="m-auto text-center">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-600">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+            <div class="p-4 m-4">
                 <input type="submit"
                     class="ml-auto cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    value=" Save " />
+                    value="Submit" />
             </div>
         </form>
-        @if ($errors->any())
-            <div class="m-auto text-center">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li class="text-red-600">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+
     </div>
 @endsection
