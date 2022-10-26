088661add3c9a0596bf07fa3cc88b857fe82f1e1
Jack Smith <jacksmithdev88@gmail.com>
2022-10-26 21:12:49 +0100

    button changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Autour+One&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Laravel</title>
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Autour+One&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-            h1 {
-                font-family: 'Autour One', cursive;
-            }
-        </style>
-    </head>
-    <body class="bg-gray-100">
-        
-        <div class="container px-24 mt-8 mx-auto">
-            <header class="flex items-center mb-12">
-                <h1 class="text-6xl">The Dog House</h1>
-                <a href="{{ route('pictures.create') }}" class="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Add your own dog
-                </a>
-            </header>
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
 
-            @yield('content')
-        </div>
-    </body>
+        h1 {
+            font-family: 'Autour One', cursive;
+        }
+    </style>
+</head>
+
+<body class="bg-gray-100">
+
+    <div class="container px-24 mt-8 mx-auto">
+        <header class="flex items-center mb-12">
+            <h1 class="text-6xl"><a href="/">The Dog House</a></h1>
+            <a href="{{ route('pictures.create') }}"
+                class="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Add your own dog
+            </a>
+        </header>
+
+        @yield('content')
+    </div>
+</body>
+
 </html>
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,21 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="bg-white p-4 w-2/3 mx-auto">
+    <div class="bg-white p-4 w-2/3 m-auto">
         <h2 class="text-center text-2xl">Upload your dog!</h2>
-        <form action="/pictures" method="post" enctype="multipart/form-data">
+        <form action="/pictures" class="m-auto flex flex-col text-center" method="post" enctype="multipart/form-data">
             @csrf
-            Name:
-            <br />
-            <input type="text" name="name" />
-            <br /><br />
-            File:
-            <br />
-            <input type="file" name="image" />
-            <input type="submit" value=" Save " />
+            <div class="p-4">
+                <label for="name">Name:</label>
+                <input type="text" name="name" />
+            </div>
+            <div class="p-4">
+                <label for="image">File:</label>
+                <input type="file" name="image" />
+            </div>
+            <div class="p-4">
+                <input type="submit"
+                    class="ml-auto cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    value=" Save " />
+            </div>
         </form>
         @if ($errors->any())
-            <div class="alert alert-danger">
+            <div class="m-auto text-center">
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li class="text-red-600">{{ $error }}</li>
